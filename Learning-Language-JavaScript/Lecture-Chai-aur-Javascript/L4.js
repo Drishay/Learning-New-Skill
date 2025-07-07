e3e3c0e179e95cbdffbca5d614a62de71b3cc511
Drishay <153807776+Drishay@users.noreply.github.com>
2025-07-07 16:07:37 +0530

    updates

---
+++ Learning-Language-JavaScript/Lecture-Chai-aur-Javascript/L4.js
@@ -0,0 +1,7 @@
+const accountId = 1212
+let accountEmail = "dri@gmail.com"
+var accountPass = 1231
+accountCity = "Noida"
+
+console.log(accountCity)
+console.table([accountId, accountEmail, accountPass, accountCity])
